048ca1f87061aa990fc5b535bca34905262feb8e
Daniel de Rauglaudre <daniel.roglo@free.fr>
2026-06-02 22:58:14 +0200

    -

--- rob_position.ml
+++ rob_position.ml
@@ -1,9 +1,11 @@
 (* $Id: rob_position.ml,v 1.1 2010/05/08 20:12:31 deraugla Exp $ *)
 
+(*
 type position = { row : int; col : int };;
+*)
 
-/*
+(*
 type ComparePos = struct type t = position let compare = compare end
-*/
 module PosMap = Map.Make (ComparePos)
 module PosSet = Set.Make (ComparePos)
+*)
