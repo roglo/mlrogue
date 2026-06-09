f22eb0de2b9c75056ea0ae54dd243e7233c8dc1c
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2026-06-09 16:58:17 +0200

    -

--- rob_action.mli
+++ rob_action.mli
@@ -1,23 +1,23 @@
 (* $Id: rob_action.mli,v 1.5 2010/05/25 16:48:27 deraugla Exp $ *)
 
-open Rob_def;
-open Rob_misc;
-open Rob_position;
+#open "rob_def";;
+#open "rob_misc";;
+#open "rob_position";;
 
-type result = (command * next_action * option move);
+type result == command * next_action * move option;;
 
 value run_away_if_possible :
-  game -> bool -> list move -> move -> option (move * list move);
+  game -> bool -> move list -> move -> (move * move list) option;;
 
-value stop_paradise : t -> _;
-value start_drop_scare : position -> char -> drop_scare;
+value stop_paradise : t -> 'a;;
+value start_drop_scare : position -> char -> drop_scare;;
 
-value start_search : game -> t -> graph -> result;
+value start_search : game -> t -> graph -> result;;
 
 value go_to_stairs :
-  game -> t -> graph -> position -> position -> bool -> result;
-value random_move : game -> position -> next_action -> result;
+  game -> t -> graph -> position -> position -> bool -> result;;
+value random_move : game -> position -> next_action -> result;;
 
-value slow_down : game -> t -> unit;
+value slow_down : game -> t -> unit;;
 
-value apply : game -> t -> string -> result;
+value apply : game -> t -> string -> result;;
