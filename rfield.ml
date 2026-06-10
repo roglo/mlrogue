2e1132c729184497f2e3c9d9b542245502912e83
Daniel de Rauglaudre <daniel.roglo@free.fr>
2026-06-10 06:03:33 +0200

    -

--- rfield.ml
+++ rfield.ml
@@ -1,46 +1,58 @@
 (* $Id: rfield.ml,v 1.4 2010/05/03 17:12:57 deraugla Exp $ *)
 
-type player_species =
-  [ PSsocket of Unix.file_descr
-  | PSrobot of Robot.t
-  | PShuman ]
-;
-
 type env =
-  [ Earray of array int
-  | Ebackup of option (string * int)
+    Earray of int vect
+  | Ebackup of (string * int) option
   | Ebool of bool
   | Eint of int
   | Eplayer of player_species
-  | Erandom of option Random.State.t ]
-;
+  | Erandom of int(*Random.State.t*) option
+;;
 
-value f_array =
-  Efield.make_fun "an array"
-    (fun [ Earray x -> Some x | _ -> None ], fun x -> Earray x)
-;
+let f_array =
+  efield__make_fun "an array"
+    ((function
+        Earray x -> Some x
+      | _ -> None),
+     (fun x -> Earray x))
+;;
 
-value f_backup =
-  Efield.make_fun "a backup"
-    (fun [ Ebackup x -> Some x | _ -> None ], fun x -> Ebackup x)
-;
+let f_backup =
+  efield__make_fun "a backup"
+    ((function
+        Ebackup x -> Some x
+      | _ -> None),
+     (fun x -> Ebackup x))
+;;
 
-value f_bool =
-  Efield.make_fun "a bool"
-    (fun [ Ebool x -> Some x | _ -> None ], fun x -> Ebool x)
-;
+let f_bool =
+  efield__make_fun "a bool"
+    ((function
+        Ebool x -> Some x
+      | _ -> None),
+     (fun x -> Ebool x))
+;;
 
-value f_int =
-  Efield.make_fun "an int"
-    (fun [ Eint x -> Some x | _ -> None ], fun x -> Eint x)
-;
+let f_int =
+  efield__make_fun "an int"
+    ((function
+        Eint x -> Some x
+      | _ -> None),
+     (fun x -> Eint x))
+;;
 
-value f_player_species =
-  Efield.make_fun "a player species"
-    (fun [ Eplayer x -> Some x | _ -> None ], fun x -> Eplayer x)
-;
+let f_player_species =
+  efield__make_fun "a player species"
+    ((function
+        Eplayer x -> Some x
+      | _ -> None),
+     (fun x -> Eplayer x))
+;;
 
-value f_random =
-  Efield.make_fun "a random"
-    (fun [ Erandom x -> Some x | _ -> None ], fun x -> Erandom x)
-;
+let f_random =
+  efield__make_fun "a random"
+    ((function
+        Erandom x -> Some x
+      | _ -> None),
+     (fun x -> Erandom x))
+;;
--- rfield.mli
+++ rfield.mli
@@ -1,16 +1,18 @@
 (* $Id: rfield.mli,v 1.4 2010/05/03 17:12:57 deraugla Exp $ *)
 
-type env = 'abstract;
+type env;;
 
 type player_species =
-  [ PSsocket of Unix.file_descr
-  | PSrobot of Robot.t
-  | PShuman ]
-;
+    PSsocket of unix__file_descr
+(*
+  | PSrobot of robot__t
+*)
+  | PShuman
+;;
 
-value f_array : Efield.field_fun env (array int);
-value f_backup : Efield.field_fun env (option (string * int));
-value f_bool : Efield.field_fun env bool;
-value f_int : Efield.field_fun env int;
-value f_player_species : Efield.field_fun env player_species;
-value f_random : Efield.field_fun env (option Random.State.t);
+value f_array : (env, int vect) efield__field_fun;;
+value f_backup : (env, (string * int) option) efield__field_fun;;
+value f_bool : (env, bool) efield__field_fun;;
+value f_int : (env, int) efield__field_fun;;
+value f_player_species : (env, player_species) efield__field_fun;;
+value f_random : (env, int(*Random.State.t*) option) efield__field_fun;;
