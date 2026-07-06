a72887c2aacc79e2bd2f850795f005f53a20c6a9
Daniel de Rauglaudre <daniel.roglo@free.fr>
2026-07-06 14:34:31 +0200

    -

--- sparse.ml
+++ sparse.ml
@@ -1,14 +1,20 @@
 let int_of_digit c = char__int_of_char c - char__int_of_char `0`;;
 
-let rec parse_int_loop n =
+let rec parse_uint_loop n =
   function
-    [< '`0`..`9` as c; (parse_int_loop (10 * n + int_of_digit c)) m >] -> m
+    [< '`0`..`9` as c; (parse_uint_loop (10 * n + int_of_digit c)) m >] -> m
   | [< >] -> n
+;;
+
+let parse_uint =
+  function
+  | [< '`0`..`9` as c; (parse_uint_loop (int_of_digit c)) m >] -> m
 ;;
 
 let parse_int =
   function
-  | [< '`0`..`9` as c; (parse_int_loop (int_of_digit c)) m >] -> m
+  | [< '`-`; parse_uint i >] -> -i
+  | [< parse_uint i >] -> i
 ;;
 
 let parse_id_char =
